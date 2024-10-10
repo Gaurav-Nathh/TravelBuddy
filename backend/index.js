127054906276e5faee2fc57d67bca4d27e8c9412
Gaurav <gauravnath725@gmail.com>
2024-10-10 10:05:07 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import { configDotenv } from "dotenv";
+import express from "express";
+import { connectDb } from "./db/connectDB.js";
+
+configDotenv();
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World 123!");
+});
+
+app.use("/api/auth",);
+app.listen(PORT, () => {
+  console.log(`Sever is running on port ${PORT}`);
+  connectDb();
+});
---
+++ backend/routes/authroute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+  
+})
