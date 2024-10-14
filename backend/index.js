2546db8e5270161f513f0f3f33c45b4f45116454
Gaurav <gauravnath725@gmail.com>
2024-10-14 09:34:17 +0530

    Third Push

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 import { configDotenv } from "dotenv";
 import express from "express";
 import { connectDb } from "./db/connectDB.js";
+import authroute from "./routes/authroute.js"
 
 configDotenv();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Travel Buddy! Hello mayank");
 });
 
-// app.use("/api/auth",);
+app.use("/api/auth", authroute);
+
 app.listen(PORT, () => {
   console.log(`Sever is running on port ${PORT}`);
   connectDb();
--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { configDotenv } from "dotenv";
-configDotenv();
+import { signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  
-})
+router.post("/signup", signup);
+
+// router.get("/login", login);
+
+// router.get("/logout", logout);
+
+export default router;
+
